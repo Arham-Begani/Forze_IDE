17be30327e9e9fa9144db0eb0466d1f1417af607
Arham Begani <arhambegani2@gmail.com>
2026-06-13 18:23:50 +0530

    style: add Prettier configuration for code formatting

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+# Prettier Configuration
+
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+};
