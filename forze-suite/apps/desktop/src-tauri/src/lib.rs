74292e14f9e0905f5094b4ae8a789e9f27565636
Arham Begani <arhambegani2@gmail.com>
2026-06-13 18:37:38 +0530

    feat: update Tauri backend library implementation

--- forze-suite/apps/desktop/src-tauri/src/lib.rs
+++ forze-suite/apps/desktop/src-tauri/src/lib.rs
@@ -354,6 +354,8 @@ pub fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .manage(WatcherState::default())
         .manage(PtyState::default())
         .setup(|app| {
